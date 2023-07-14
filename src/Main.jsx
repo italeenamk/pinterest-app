8ed045c03c515286d7ba235b6efe2f8ca7a048d6
italeenamk <italeenamk@gmail.com>
2023-07-14 17:53:41 +0500

    add home file in main

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Home from "./pages/home/Home";
 
 
 const Main = () => {
   return(
       <>
           <h1>hrllo</h1>
+          <Home/>
       </>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => {
   return(
       <>
+          <h2>home</h2>
       </>
   )
 }
